8cdfa0471f6a15812f0cb5b148e5c4b87198522f
Ivancf04 <jcota_22@alu.uabcs.mx>
2024-12-06 06:38:04 -0600

    Add files via upload

--- Examen_HTML_CSS/src/views/layouts/header.php
+++ Examen_HTML_CSS/src/views/layouts/header.php
@@ -1,65 +1,20 @@
-<?php 
-    require __DIR__.'/../../helpers/functions.php';
-    $products = getProducts();
+<?php
+     $user = $_GET['userId'];
+     $idCarrito = $_GET['carrito'];
 ?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?=ASSETS_URL?>/css/index.css">
-
-    <title>Parfum</title>
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
-    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
-</head>
 <header>
         <div id="caja_nombre">
             <!--Nombre del sitio-->
-            <h1 class="titulo">Parfum</h1>
+            <a style="text-decoration:none; color:inherit;" href="./menu.php?page=1&userId=<?=$user?>&carrito=<?=$idCarrito?>"><h1 class="titulo">Parfum</h1></a>
         </div>
 
         <div id="caja_texto">
             <!--Opciones de nav-->
-            <div id="base" class="opcion">
-                <h2 class="opc">Base</h2>
-                <ul class="menu">
-                    <li>Madera</li>
-                    <li>Mineral</li>
-                    <li>Flor</li>
-                    <li>Agua</li>
-                </ul>
-            </div>
-            <div id="temporada" class="opcion">
-                <h2 class="opc">Temporada</h2>
-                <ul class="menu">
-                    <li>Primavera</li>
-                    <li>Verano</li>
-                    <li>Otoño</li>
-                </ul>
-            </div>
-            <div id="paquetes" class="opcion">
-                <h2 class="opc">Paquetes</h2>
-                <ul class="menu">
-                    <li>Dia</li>
-                    <li>Noche</li>
-                    <li>Ambos</li>
-                </ul>
-            </div>
-
-            <div id="menu_hamburguesa">
-                <button id="menu"><img src="<?=ASSETS_URL?>/imgs/menu.png" alt="img"></button>
-            </div>
+            <form id="base" class="opcion" method="POST" action="<?=SRC_URL?>/controllers/controlBus.php">
+                <input type="hidden" name="idUsuario" value="<?=$user?>"></input>
+                <input type="hidden" name="idCarrito" value="<?=$idCarrito?>"></input>
+                <input id="inBus" type="text" name="texto" placeholder="Ingresa el nombre del producto">
+                <button id="buBus" type="submit"><img src="<?=ASSETS_URL?>/imgs/lupa.png" alt="img"></button> 
+            </form>
         </div>
 
-
-        <div id="caja_opciones">
-            <button><img src=  "<?=ASSETS_URL?>/imgs/carrito-de-compras.png" alt="img"></button>
-        </div>
-    </header>
