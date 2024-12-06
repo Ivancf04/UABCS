c813f90b521068922b392886ee36a54d030bb1bc
Ivancf04 <jcota_22@alu.uabcs.mx>
2024-12-06 06:36:43 -0600

    Add files via upload

--- Examen_HTML_CSS/src/config/config.php
+++ Examen_HTML_CSS/src/config/config.php
@@ -1,11 +1,12 @@
 <?php 
 
 return [
-    'base_url' => 'http://localhost:80/UABCS/Examen_HTML_CSS/public',
-    'assets_url' => 'http://localhost:80/UABCS/Examen_HTML_CSS/public/assets',
+    'base_url' => 'http://localhost:80/Examen_HTML_CSS/public',
+    'assets_url' => 'http://localhost:80/Examen_HTML_CSS/public/assets',
+    'src_url' => 'http://localhost:80/Examen_HTML_CSS/src',
     'db' => [
         'host' => '127.0.0.1',
-        'name' => 'parfum',
+        'name' => 'if0_37659505_parfum',
         'user' => 'root',
         'password' => ''
     ]
